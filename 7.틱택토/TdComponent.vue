0aec18d027b118f586ef06da2c54e13fbd3e7fa6
zerocho <zerohch0@gmail.com>
2019-07-15 23:52:57 +0900

    Update: 틱택토

---
+++ 7.틱택토/TdComponent.vue
@@ -0,0 +1,55 @@
+<template>
+  <td @click="onClickTd">{{cellData}}</td>
+</template>
+
+<script>
+  export default {
+    props: {
+      cellData: String,
+      rowIndex: Number,
+      cellIndex: Number,
+    },
+    methods: {
+      onClickTd() {
+        const rootData = this.$root.$data;
+        this.$set(rootData.tableData[this.rowIndex], this.cellIndex, rootData.turn);
+
+        let win = false;
+        if (rootData.tableData[this.rowIndex][0] === rootData.turn && rootData.tableData[this.rowIndex][1] === rootData.turn && rootData.tableData[this.rowIndex][2] === rootData.turn) {
+          win = true;
+        }
+        if (rootData.tableData[0][this.cellIndex] === rootData.turn && rootData.tableData[1][this.cellIndex] === rootData.turn && rootData.tableData[2][this.cellIndex] === rootData.turn) {
+          win = true;
+        }
+        if (rootData.tableData[0][0] === rootData.turn && rootData.tableData[1][1] === rootData.turn && rootData.tableData[2][2] === rootData.turn) {
+          win = true;
+        }
+        if (rootData.tableData[0][2] === rootData.turn && rootData.tableData[1][1] === rootData.turn && rootData.tableData[2][0] === rootData.turn) {
+          win = true;
+        }
+
+        if (win) { // 이긴 경우: 3줄 달성
+          rootData.winner = rootData.turn;
+          rootData.turn = 'O';
+          rootData.tableData = [['', '', ''], ['', '', ''], ['', '', '']];
+        } else { // 무승부
+          let all = true; // all이 true면 무승부라는 뜻
+          rootData.tableData.forEach((row) => { // 무승부 검사
+            row.forEach((cell) => {
+              if (!cell) {
+                all = false;
+              }
+            });
+          });
+          if (all) { // 무승부
+            rootData.winner = '';
+            rootData.turn = 'O';
+            rootData.tableData = [['', '', ''], ['', '', ''], ['', '', '']];
+          } else {
+            rootData.turn = rootData.turn === 'O' ? 'X' : 'O';
+          }
+        }
+      }
+    }
+  };
+</script>
---
+++ 7.틱택토/TicTacToe.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div>{{turn}}님의 턴입니다.</div>
+    <table-component :table-data="tableData" />
+    <div v-if="winner">{{winner}}님의 승리!</div>
+  </div>
+</template>
+
+<script>
+  import TableComponent from './TableComponent';
+
+  export default {
+    components: {
+      TableComponent,
+    },
+    data() {
+      return {
+        tableData: [
+          ['', '', ''],
+          ['', '', ''],
+          ['', '', ''],
+        ],
+        turn: 'O',
+        winner: '',
+      };
+    },
+    methods: {
+      onChangeData() {
+        // this.tableData[1][0] = 'X'; 작동하지 않음
+        this.$set(this.tableData[1], 0, 'X'); // Vue.set과 동일
+      }
+    },
+  };
+</script>
+
+<style>
+  table {
+    border-collapse: collapse;
+  }
+  td {
+    border: 1px solid black;
+    width: 40px;
+    height: 40px;
+    text-align: center;
+  }
+</style>
