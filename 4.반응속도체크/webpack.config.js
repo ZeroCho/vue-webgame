bda7e365494641bfe2e26ac58e798c94bf6b059e
ZeroCho <zerohch0@gmail.com>
2020-11-07 21:41:54 +0900

    Update: hot reloading

--- 4.반응속도체크/webpack.config.js
+++ 4.반응속도체크/webpack.config.js
@@ -33,6 +33,9 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.join(__dirname, 'dist'),
-    publicPath: '/dist',
+    publicPath: '/dist/',
   },
+  devServer: {
+    publicPath: '/dist/',
+  }
 };
