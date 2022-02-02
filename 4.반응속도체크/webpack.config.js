54b3c5a5129d422450e38dfc56d3501a379e3478
zerocho <zerohch0@gmail.com>
2022-02-03 01:24:47 +0900

    Update: ch4 devServer 코드 최신화

--- 4.반응속도체크/webpack.config.js
+++ 4.반응속도체크/webpack.config.js
@@ -2,40 +2,42 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
-  devtool: 'eval',
-  resolve: {
-    extensions: ['.js', '.vue'],
-  },
-  entry: {
-    app: path.join(__dirname, 'main.js'),
-  },
-  module: {
-    rules: [{
-      test: /\.vue$/,
-      use: 'vue-loader',
-    }, {
-      test: /\.css$/,
-      use: [
-        'vue-style-loader',
-        {
-          loader: 'css-loader',
-          options: {
-            esModule: false,
-          }
-        },
-      ]
-    }],
-  },
-  plugins: [
-    new VueLoaderPlugin(),
-  ],
-  output: {
-    filename: '[name].js',
-    path: path.join(__dirname, 'dist'),
-    publicPath: '/dist/',
-  },
-  devServer: {
-    publicPath: '/dist/',
-  }
+    mode: 'development',
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js', '.vue'],
+    },
+    entry: {
+        app: path.join(__dirname, 'main.js'),
+    },
+    module: {
+        rules: [{
+            test: /\.vue$/,
+            use: 'vue-loader',
+        }, {
+            test: /\.css$/,
+            use: [
+                'vue-style-loader',
+                {
+                    loader: 'css-loader',
+                    options: {
+                        esModule: false,
+                    }
+                },
+            ]
+        }],
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+    ],
+    output: {
+        filename: '[name].js',
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/dist/',
+    },
+    devServer: {
+        devMiddleware: {publicPath: '/dist'},
+        static: {directory: path.resolve(__dirname)},
+        hot: true
+    }
 };
